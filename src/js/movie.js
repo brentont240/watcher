aec2a4ed66d4cc2dbc7b0e01765613563e0e65cd
Brenton Trebilcock <tre15004@byui.edu>
2022-03-10 11:11:37 -0700

    working on the movie details page

---
+++ src/js/movie.js
@@ -0,0 +1,5 @@
+import movieDetails from "./movieDetails.js";
+
+const movie = new movieDetails(1,2);
+
+movie.init();
---
+++ src/js/movieDetails.js
@@ -0,0 +1,55 @@
+
+// TODO: add a way to add to list (aimilar to add to cart in the other thing)
+export default class movieDetails{
+    constructor(movieId, dataSource) {
+        this.movieId = movieId;
+        this.movie = {};
+        this.starRating = "";
+        this.dataSource = dataSource;
+    }
+
+    // look at ways to include more red into this!
+    renderMovieDetails(){
+      return `<section class="movie-details center-text">
+        <h1>${this.movie.name}</h1>
+        <img src="" alt="">
+        <p class="rating">${this.starRating}</p>
+        <p class="description">${this.movie.Desc}</p>
+      </section>
+      `;
+    }
+
+    getStarRating(){
+      // add full stars until you get to the rating
+      for (let i = 0; i < this.movie.Rate; i++) {
+        this.starRating += "&#9733";
+      }
+      // add in blank stars to fill in the rest
+      if (this.movie.Rate != 5){
+        for (let i = this.movie.Rate; i < 5; i++) {
+          this.starRating += "&#9734;";
+        }
+      }
+    }
+
+    async init(){
+      // TODO: use something like this to get the data
+      // this.movie = await this.dataSource.findMovieById(this.movieId);
+
+      // testing
+      this.movie =       {
+        "id": "0",
+        "name": "Interstellar",
+        "url": "https://something",
+        "Desc": "Best movie ever made",
+        "Rate": "5",
+        "year": "20??",
+        "time": "2hr ?min",
+        "genre": "Sci-Fi"
+      };
+
+      this.getStarRating();
+      document.querySelector("main").innerHTML = this.renderMovieDetails();
+    }
+
+}
