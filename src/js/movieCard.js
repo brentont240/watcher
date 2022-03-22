bd2680631f728960fb2c544c372408a2711f7a85
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-03-22 13:37:48 -0600

    styled cards

--- src/js/movieCard.js
+++ src/js/movieCard.js
@@ -10,7 +10,7 @@ export default class MovieCard {
         // one movie is passed in and this function generates html for that movie.
         let card = document.createElement("div");
         card.innerHTML = `<div class="movie-card">
-        <a href="signup.html"> <img class="movie-img" alt="movie art" src="../interstellar.jpeg"></a>
+        <a href="signup.html"> <img class="movie-img" alt="movie art" src="${movie.url}"></a>
         <div class="movie-text">
             <a href="signup.html"> <p class="name">${movie.name}</p> </a>
             <p class="genre">${movie.genre}</p>
@@ -30,6 +30,7 @@ export default class MovieCard {
             movie.id = item.id;
             movie.genre = item.genre;
             movie.desc = item.Desc;
+            movie.url = item.url;
             this.movieCardTemplate(movie);
         })
     }
