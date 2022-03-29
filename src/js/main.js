ab8d9bb454575ae88633c07d404656c7ac6c4453
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-03-29 12:48:03 -0600

    add movie form

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,12 @@ import { loadHeaderFooter } from "./utils.js";
 const data = new ExternalServices
 loadHeaderFooter();
 data.getData();
+
+var slider = document.getElementById("minute_slider");
+var output = document.getElementById("movie_length");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
