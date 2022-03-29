0ac176c2c64b420211e15940deb21e2d8a051b09
Brenton <brentontreb@gmail.com>
2022-03-29 12:57:38 -0600

    Added hamburger button to smaller screens and moved some css to their own files

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import ExternalServices from "./externalServices.js";
 import { loadHeaderFooter } from "./utils.js";
 
-const data = new ExternalServices
+const data = new ExternalServices;
 loadHeaderFooter();
 data.getData();
+
+// for hamburger menu on mobile
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,39 +1,48 @@
 // converts the response to text
 function convertToText(res) {
-    if (res.ok) {
-      return res.text();
-    } else {
-      throw new Error("Bad Response");
-    }
+  if (res.ok) {
+    return res.text();
+  } else {
+    throw new Error("Bad Response");
   }
+}
 
 // loads in a template
 export async function loadTemplate(path) {
-    const html = await fetch(path).then(convertToText);
-    const template = document.createElement("template");
-    template.innerHTML = html;
-    return template;
-  }
+  const html = await fetch(path).then(convertToText);
+  const template = document.createElement("template");
+  template.innerHTML = html;
+  return template;
+}
 
 // helps with rendering with templates
 export function renderWithTemplate(template, parent, data, callback) {
-    let clone = template.content.cloneNode(true);
-    if (callback) {
-      clone = callback(clone, data);
-    }
-    parent.appendChild(clone);
+  let clone = template.content.cloneNode(true);
+  if (callback) {
+    clone = callback(clone, data);
+  }
+  parent.appendChild(clone);
 }
 
 // loads the header and the footer
 export async function loadHeaderFooter() {
-    const head = await loadTemplate("../partials/header.html");
-    const foot = await loadTemplate("../partials/footer.html");
-    const header = document.getElementById("main-header");
-    const footer = document.getElementById("main-footer");
-    renderWithTemplate(head, header);
-    renderWithTemplate(foot, footer);
-    document.querySelector(".year").innerHTML = new Date().getFullYear();
+  const head = await loadTemplate("../partials/header.html");
+  const foot = await loadTemplate("../partials/footer.html");
+  const header = document.getElementById("main-header");
+  const footer = document.getElementById("main-footer");
 
+  // render the header and footer
+  renderWithTemplate(head, header);
+  renderWithTemplate(foot, footer);
+  const hamburgerMenu = document.getElementById("hamburger-menu");
+  const navUl = document.getElementById("navbar-ul");
+  // add functionality to the hamburger menu
+  hamburgerMenu.addEventListener("click", () => {
+    navUl.classList.toggle("show");
+    hamburgerMenu.classList.toggle("open");
+  });
+  // add the current year to the copyright year in the footer
+  document.querySelector(".year").innerHTML = new Date().getFullYear();
 }
 
 export function getParams(param) {
