5d937c1e68b1636e75292543ff73d859adbc009b
Brenton <brentontreb@gmail.com>
2022-04-04 13:54:31 -0600

    Moved code around to prevent errors on pages. Moved code to only be on its relevant page

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,7 @@
-import ExternalServices from "./externalServices.js";
+// import ExternalServices from "./externalServices.js";
 import { loadHeaderFooter } from "./utils.js";
 
-const data = new ExternalServices;
+
 loadHeaderFooter();
-data.getData();
 
-var slider = document.getElementById("minute_slider");
-var output = document.getElementById("movie_length");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-};
 
