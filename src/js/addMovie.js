907759fc6b31ac20c16af3d5f89029c04afd74dd
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-04-04 11:17:41 -0600

    cors

--- src/js/addMovie.js
+++ src/js/addMovie.js
@@ -18,7 +18,7 @@ form.addEventListener("submit" , (e) => {
     const options = {
         method: "POST",
         headers: {
-          "Access-Control-Allow_Origin":  "https://brentont240.github.io/watcher/src/html/add-movie.html",
+          "Access-Control-Allow_Origin":  "https://brentont240.github.io",
           "Content-Type": "application/json",
         },
         body: JSON.stringify(json),
